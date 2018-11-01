2762a17a5943f69aa19761df70931094ecbaf3da
CameronSchmidt <cameron.schmidt@sonarsource.com>
2018-11-01 13:45:35 +0100

    Correct build errors

--- app-java/src/main/java/example/HelloWorld.java
+++ app-java/src/main/java/example/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
   public void coveredByUnitTest() {
     System.out.println("coveredByUnitTest1");
     System.out.println("coveredByUnitTest2");
-    System.out.println("Covered in new branch")
+    System.out.println("Covered in new branch");
   }
 
   public void coveredByIntegrationTest() {
@@ -16,7 +16,7 @@ public class HelloWorld {
 
   public void notCovered() {
     System.out.println("notCovered");
-    System.out.println("Not covered in new branch")
+    System.out.println("Not covered in new branch");
   }
 
 }
