94ca0897f92a9aac015a1bbcd88fbfe2d0d6eea0
CameronSchmidt <cameron.schmidt@sonarsource.com>
2018-11-01 16:03:17 +0100

    Undo branch changes

--- app-java/src/main/java/example/HelloWorld.java
+++ app-java/src/main/java/example/HelloWorld.java
@@ -5,14 +5,12 @@ public class HelloWorld {
   public void coveredByUnitTest() {
     System.out.println("coveredByUnitTest1");
     System.out.println("coveredByUnitTest2");
-    System.out.println("Covered in Develop branch");
   }
 
   public void coveredByIntegrationTest() {
     System.out.println("coveredByIntegrationTest1");
     System.out.println("coveredByIntegrationTest2");
     System.out.println("coveredByIntegrationTest3");
-    System.out.println("Also covered in Develop branch");
   }
 
   public void notCovered() {
